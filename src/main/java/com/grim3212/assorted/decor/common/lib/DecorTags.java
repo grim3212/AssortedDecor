9315346e8f2b957616b66d502d2180dadc2b41af
Dakota Pierone <dakotapierone@yahoo.com>
2022-03-02 21:31:54 -0800

    Update to 1.18.2

--- src/main/java/com/grim3212/assorted/decor/common/lib/DecorTags.java
+++ src/main/java/com/grim3212/assorted/decor/common/lib/DecorTags.java
@@ -1,18 +1,18 @@
 package com.grim3212.assorted.decor.common.lib;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.common.Tags.IOptionalNamedTag;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 
 public class DecorTags {
 
 	public static class Items {
-		public static final IOptionalNamedTag<Item> BUCKETS_WATER = forgeTag("buckets/water");
-		public static final IOptionalNamedTag<Item> INGOTS_ALUMINUM = forgeTag("ingots/aluminum");
+		public static final TagKey<Item> BUCKETS_WATER = forgeTag("buckets/water");
+		public static final TagKey<Item> INGOTS_ALUMINUM = forgeTag("ingots/aluminum");
 
-		private static IOptionalNamedTag<Item> forgeTag(String name) {
-			return ItemTags.createOptional(new ResourceLocation("forge", name));
+		private static TagKey<Item> forgeTag(String name) {
+			return ItemTags.create(new ResourceLocation("forge", name));
 		}
 	}
 }
